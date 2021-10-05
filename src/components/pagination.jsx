10f450b86e60bb926909dfbf5b49fe5af9eb92c0
NewExample <timurmanilov12@mail.ru>
2021-10-05 13:56:57 +0300

    paginations

---
+++ src/components/pagination.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import _ from "lodash";
+
+const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }) => {
+  const pageCount = Math.ceil(itemsCount / pageSize);
+  const pages = _.range(1, pageCount + 1);
+  if (pageCount === 1) return null;
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li
+            className={"page-item " + (page === currentPage ? "active" : "")}
+            key={page}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import User from "./user";
+import Pagination from "./pagination";
+import { paginate } from "../utils/paginate";
 const Users = ({ users, onDelete }) => {
+  const count = users.length;
+  const pageSize = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+  const handlePageChange = (pageIndex) => {
+    console.log("page", pageIndex);
+    setCurrentPage(pageIndex);
+  };
+  const userCrop = paginate(users, currentPage, pageSize);
   return (
     <>
       {users.length > 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Кол-во встреч</th>
-              <th scope="col">Оценка</th>
-              <th scope="col">Избранное</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          {users.map((user) => (
-            <User key={user._id} user={user} onDelete={onDelete} />
-          ))}
-        </table>
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Профессия</th>
+                <th scope="col">Кол-во встреч</th>
+                <th scope="col">Оценка</th>
+                <th scope="col">Избранное</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+
+            {userCrop.map((user) => (
+              <User key={user._id} user={user} onDelete={onDelete} />
+            ))}
+          </table>
+          <Pagination
+            itemsCount={count}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+          />
+        </>
       )}
     </>
   );
